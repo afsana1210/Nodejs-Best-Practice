5d2518c16c4908c95941529ebaa53fad0eed339b
afsana1210 <afsana1210@gmail.com>
2021-02-07 11:36:40 +0530

    two module of shape area of cylender and sphere

---
+++ nodejs/area.js
@@ -0,0 +1,9 @@
+exports.areac=function areaofcylender=(r,h){
+     area=2*3.14*r*h+2*3.14*r*r;
+    return area
+}
+
+exports.areas=function areaofsphere(r){
+    area=4*3.14*r*r;
+    return area;
+}
